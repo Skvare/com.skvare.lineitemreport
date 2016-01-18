bcc2727b2593a53687ed11bdc5a62c28dba10d47
Jeremy Proffitt <jeremy@skvare.com>
2016-01-18 15:46:58 -0500

    update entityTable name for civicrm_participant

--- CRM/Lineitemreport/Report/Form/LineItem.php
+++ CRM/Lineitemreport/Report/Form/LineItem.php
@@ -443,7 +443,7 @@ ORDER BY  cv.label
     $this->_from = "
         FROM civicrm_{$this->_entity} {$this->_aliases['civicrm_'.$this->_entity]}
              LEFT JOIN civicrm_contact {$this->_aliases['civicrm_contact']}
-                    ON {$this->_aliases['civicrm_'.$this->_entity]}.contact_id  = {$this->_aliases['civicrm_contact']}.id
+                    ON ({$this->_aliases['civicrm_'.$this->_entity]}.contact_id  = {$this->_aliases['civicrm_contact']}.id  )
              {$this->_aclFrom}
              LEFT JOIN civicrm_address {$this->_aliases['civicrm_address']}
                     ON {$this->_aliases['civicrm_contact']}.id = {$this->_aliases['civicrm_address']}.contact_id AND
@@ -502,7 +502,7 @@ ORDER BY  cv.label
       switch($this->_entity) {
         case 'participant':
           $entityId = $this->_submitValues['event_id_value'];
-          $entityTable = 'civicrm_event';
+          $entityTable = 'civicrm_participant';
           break;
 
         case 'membership':
